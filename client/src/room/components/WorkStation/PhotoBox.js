e98b368f13e53600be89e64ed5dbe544c915b22b
nuday1 <kz334459@gmail.com>
2023-02-11 03:42:44 +0900

    230211 UI 디자인중

--- client/src/room/components/WorkStation/PhotoBox.js
+++ client/src/room/components/WorkStation/PhotoBox.js
@@ -5,19 +5,31 @@ import "./PhotoBox.css";
 
 const PhotoBox = (props) => {
   return (
-    <div className="closed_photobox_group">
-      <div className="closed_photobox">
-        <div className="closed_preview_photos"></div>
-        <div className="open_and_work">
-          <button onClick={props.openBox} className="open_button">
-            <img src={Arrow} className="Icon-Arrow" />
-          </button>
-          <div className="work_layout">
-            <div className="work_button_group">
-              <div className="work_button">
-                <label className="work_label">작업하기</label>
-              </div>
-            </div>
+    // <div className="closed_photobox_group">
+    //   <div className="closed_photobox">
+    //     <div className="closed_preview_photos"></div>
+    //     <div className="open_and_work">
+    //       <button onClick={props.openBox} className="open_button">
+    //         <img src={Arrow} className="Icon-Arrow" />
+    //       </button>
+    //       <div className="work_layout">
+    //         <div className="work_button_group">
+    //           <div className="work_button">
+    //             <label className="work_label">작업하기</label>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div class="PhotoBox">
+      <div class="PhotosButton">
+        <div class="upload_Photos">
+          <div class="upload_Photos_layout">
+            <div class="upload_img"></div>
+            <div class="upload_img"></div>
+            <div class="upload_img"></div>
+            <div class="upload_img"></div>
           </div>
         </div>
       </div>
--- client/src/room/components/WorkStation/WorkStation.js
+++ client/src/room/components/WorkStation/WorkStation.js
@@ -8,24 +8,10 @@ import Effects from "./Effects";
 import "./WorkStation.css";
 
 const WorkStation = () => {
-  const [photoBoxIsOpen, setPhotoBoxIsOpen] = useState(false);
-
-  const openBoxHandler = () => {
-    setPhotoBoxIsOpen(true);
-  };
-
-  const closeBoxHandler = () => {
-    setPhotoBoxIsOpen(false);
-  };
-
   return (
     <React.Fragment>
-      <div className="ROOM-BODY-CANVAS">
-        {photoBoxIsOpen ? (
-          <OpenedPhotoBox closeBox={closeBoxHandler} />
-        ) : (
-          <PhotoBox openBox={openBoxHandler} />
-        )}
+      <div className="ROOM-BODY-WorkStation">
+        <PhotoBox />
         <Canvas />
         <Effects />
       </div>

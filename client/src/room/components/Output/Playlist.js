0d43ddd1e33a0401c2a127d8ae753c6905f38121
nuday1 <kz334459@gmail.com>
2023-02-15 22:24:43 +0900

    230215 playlist context화 진행

--- client/src/room/components/Output/Playlist.js
+++ client/src/room/components/Output/Playlist.js
@@ -1,19 +1,24 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useContext } from "react";
+// import axios from "axios";
 
-import PlaylistShow from "./PlaylistShow";
+import PlaylistContext from "../../../shared/context/playlist-context";
+// import PlaylistShow from "./PlaylistShow";
 import "./Playlist.css";
 
 const Playlist = () => {
-  const [playView, setPlayView] = useState({});
-  const [playCheck, setPlayCheck] = useState(true);
+  const playlistCtx = useContext(PlaylistContext);
+  const playlistView = playlistCtx.playlist;
+  console.log(playlistView);
+
+  // const [playView, setPlayView] = useState({});
+  // const [playCheck, setPlayCheck] = useState(true);
   // const [check2, setcheck2] = useState(true);
 
-  useEffect(() => {
-    axios.get("http://localhost:5000/output/playlist").then((res) => {
-      setPlayView(res.data);
-    });
-  }, [playCheck]);
+  // useEffect(() => {
+  //   axios.get("http://localhost:5000/output/playlist").then((res) => {
+  //     setPlayView(res.data);
+  //   });
+  // }, [playlistCtx.playlist]);
 
   // useEffect(() => {
   //   axios.get("http://localhost:5000/photoBox/sendimages").then((res) => {
@@ -21,15 +26,50 @@ const Playlist = () => {
   //   });
   // }, [check2]);
 
-  const changePlayCheck = () => {
-    setPlayCheck(playCheck ? false : true);
-  };
+  // const changePlayCheck = () => {
+  //   setPlayCheck(playCheck ? false : true);
+  // };
 
   // const changecheck2 = () => {
   //   setcheck2(check2 ? false : true);
   // };
 
-  return <PlaylistShow playView={playView} change={changePlayCheck} />;
+  //   const selectimage = async (url) => {
+  //     axios
+  //       .post("http://localhost:5000/photoBox/clickimage", { url: url })
+  //       .then((res) => {})
+  //       .then((res) => {
+  //         props.change();
+  //       });
+  //   };
+
+  const context = Object.keys(playlistView).map((url) => {
+    if (playlistView[url] === 0) {
+      return (
+        <img
+          className="toplay_img"
+          key={url}
+          src={url}
+          alt="a"
+          //   onClick={() => selectimage(url)}
+        />
+      );
+    } else {
+      return (
+        <img
+          className="toplay_select_img"
+          key={url}
+          src={url}
+          alt="a"
+          //   onClick={() => {
+          //     selectimage(url);
+          //   }}
+        />
+      );
+    }
+  });
+
+  return <div className="playlist_layout">{context}</div>;
 };
 
 export default Playlist;

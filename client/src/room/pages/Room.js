c5d9516976a07f2f6afb93fbece764d7e4d0c4cf
nuday1 <kz334459@gmail.com>
2023-02-23 06:06:28 +0900

    충일 merge 전 병합 완료

--- client/src/room/pages/Room.js
+++ client/src/room/pages/Room.js
@@ -1,23 +1,24 @@
 import axios from "axios";
 import React, { useEffect, useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import RoomCodeContext from "../../shared/context/roomcode-context";
-import { AuthContext } from "../../shared/context/auth-context";
 
 import RoomHeader from "../components/RoomHeader/RoomHeader";
 import Contents from "./Contents";
 
 const Room = () => {
-  const navigate = useNavigate()
-  const AuthCtx = useContext(AuthContext);
+  const navigate = useNavigate();
+  const roomId = useParams().roomId;
 
   useEffect(() => {
-    axios.post("http://localhost:5000/home/check", {id:AuthCtx.rooomId}).then((res) => {
-      if (res.data === "No"){
-        navigate("/")
-      }
-    })
-  },[])
+    axios
+      .post("http://localhost:5000/home/check", { id: roomId })
+      .then((res) => {
+        if (res.data === "No") {
+          navigate("/");
+        }
+      });
+  }, []);
 
   return (
     <RoomCodeContext.Provider value={Math.floor(Math.random() * 1000)}>

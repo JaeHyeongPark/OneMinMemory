53b5eb86e12757c196d2c632499a5f484a0aabc5
ChoongilLee95 <choongil.lee95@gmail.com>
2023-03-01 09:44:39 +0900

    [fix]: warning 뜨는 부분 수정

--- client/src/room/components/WebRTC/WebRTC.js
+++ client/src/room/components/WebRTC/WebRTC.js
@@ -170,52 +170,8 @@ socket.on("iceForSending", async (data) => {
   }
 });
 
-let video_mapped = false;
-
 // 새로운 사용자 들어왔을 때 실행되는 소캣
 socket.on("makeNewPeer", (data) => {
-  if (video_mapped === false) {
-    peersFace1 = document.getElementById("peersFace1");
-    peersFace2 = document.getElementById("peersFace2");
-    peersFace3 = document.getElementById("peersFace3");
-    peersFace4 = document.getElementById("peersFace4");
-    videoFrame1 = document.getElementById("videoFrame1");
-    videoFrame2 = document.getElementById("videoFrame2");
-    videoFrame3 = document.getElementById("videoFrame3");
-    videoFrame4 = document.getElementById("videoFrame4");
-    imgTag1 = document.getElementById("imgTag1");
-    imgTag2 = document.getElementById("imgTag2");
-    imgTag3 = document.getElementById("imgTag3");
-    imgTag4 = document.getElementById("imgTag4");
-    videos = [
-      {
-        imgTag: imgTag1,
-        videoFrame: videoFrame1,
-        videoTag: peersFace1,
-        isConnected: false,
-      },
-      {
-        imgTag: imgTag2,
-        videoFrame: videoFrame2,
-        videoTag: peersFace2,
-        isConnected: false,
-      },
-      {
-        imgTag: imgTag3,
-        videoFrame: videoFrame3,
-        videoTag: peersFace3,
-        isConnected: false,
-      },
-      {
-        imgTag: imgTag4,
-        videoFrame: videoFrame4,
-        videoTag: peersFace4,
-        isConnected: false,
-      },
-    ];
-    video_mapped = true;
-  }
-
   console.log(sendingConnection.connectionState);
   console.log("새로운 친구가 왔을 때 사용되는 소캣");
   streamIdToUser[data.streamId] = data.senderId;
@@ -379,6 +335,47 @@ async function makeNewConnection() {
     console.log(e);
   }
 }
+// 태그들을 자료형에 매핑하는 함수
+const initialSetting = () => {
+  peersFace1 = document.getElementById("peersFace1");
+  peersFace2 = document.getElementById("peersFace2");
+  peersFace3 = document.getElementById("peersFace3");
+  peersFace4 = document.getElementById("peersFace4");
+  videoFrame1 = document.getElementById("videoFrame1");
+  videoFrame2 = document.getElementById("videoFrame2");
+  videoFrame3 = document.getElementById("videoFrame3");
+  videoFrame4 = document.getElementById("videoFrame4");
+  imgTag1 = document.getElementById("imgTag1");
+  imgTag2 = document.getElementById("imgTag2");
+  imgTag3 = document.getElementById("imgTag3");
+  imgTag4 = document.getElementById("imgTag4");
+  videos = [
+    {
+      imgTag: imgTag1,
+      videoFrame: videoFrame1,
+      videoTag: peersFace1,
+      isConnected: false,
+    },
+    {
+      imgTag: imgTag2,
+      videoFrame: videoFrame2,
+      videoTag: peersFace2,
+      isConnected: false,
+    },
+    {
+      imgTag: imgTag3,
+      videoFrame: videoFrame3,
+      videoTag: peersFace3,
+      isConnected: false,
+    },
+    {
+      imgTag: imgTag4,
+      videoFrame: videoFrame4,
+      videoTag: peersFace4,
+      isConnected: false,
+    },
+  ];
+};
 
 let voiceStatus = 0;
 
@@ -386,6 +383,7 @@ const WebRTC = () => {
   useEffect(() => {
     roomId = App.roomId;
     setTimeout(() => {
+      initialSetting();
       // 서버가 보낸 다른사람의 그림 데이터
       App.mainSocket.on("myCanvas", (data) => {
         if (userInfo[data.senderId]) {
@@ -527,12 +525,6 @@ const WebRTC = () => {
   );
 };
 
-//-------------- 버튼 관리------------------- 아직안씀
-// 나가기 버튼
-// function handleQuit() {
-//   socket.emit("quit", roomId, socket.id);
-//   window.location.reload();
-// }
 // 카메라 전환 버튼
 // async function handleCameraChange() {
 //   await getMedia(cameraSelect.value);
